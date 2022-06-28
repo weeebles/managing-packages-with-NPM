2691d17c35bc44f7a87f29c53b6b7526fab8e735
bleesherman <becs.leesherman@gmail.com>
2022-06-27 23:36:21 -0500

    Removing stuff to match server.js more

--- server.js
+++ server.js
@@ -3,10 +3,8 @@
  * the verification process may break
  * ***************************************************/
 
-'use strict';
 
 var bGround = require('fcc-express-bground');
-var fs = require('fs');
 var express = require('express');
 var myApp = require('./myApp');
 var app = express();
@@ -24,36 +22,7 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.use('/public', express.static(process.cwd() + '/public'));
 
-app.route('/_api/package.json')
-  .get(function(req, res, next) {
-    console.log('requested');
-    fs.readFile(__dirname + '/package.json', function(err, data) {
-      if(err) return next(err);
-      res.type('txt').send(data.toString());
-    });
-  });
-  
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
-
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
-});
-
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
-})
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){

4c63ae6ee64daa6198e56c379d71bd865094e4d9
bleesherman <becs.leesherman@gmail.com>
2022-06-27 23:29:43 -0500

    Setting up app for basic node and express

---
+++ myApp.js
@@ -0,0 +1,39 @@
+let express = require('express');
+let app = express();
+var bGround = require('fcc-express-bground');
+
+
+
+
+
+
+
+bGround.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;

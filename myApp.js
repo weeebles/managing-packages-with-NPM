08acc30a28bc38be5b8fe1f6d8511b411560b369
bleesherman <becs.leesherman@gmail.com>
2022-07-02 23:58:46 -0500

    Add POST data request api endpoint at name path

--- myApp.js
+++ myApp.js
@@ -69,7 +69,9 @@ app.get("/name", (req, res) => {
     res.json({ name: req.query.first + " " + req.query.last});
 });
 
-
+app.post("/name", function(req, res) {
+    res.json({ name: req.body.first + " " + req.body.last});
+});
 
 
 

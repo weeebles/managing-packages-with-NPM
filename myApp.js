83b9be470189163ba1f95dfa173cb3bb96c3e307
bleesherman <becs.leesherman@gmail.com>
2022-07-02 19:51:43 -0500

    Organize application function to myapp.js

--- myApp.js
+++ myApp.js
@@ -13,6 +13,13 @@ console.log("Hello World");
 
 
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+
+app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + "/public"));
 
 
 

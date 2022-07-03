8e6d442c33bd1e71a74c2206d2df6eaf08ace51c
bleesherman <becs.leesherman@gmail.com>
2022-07-02 20:03:36 -0500

    Add json route with message

--- myApp.js
+++ myApp.js
@@ -17,11 +17,14 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-
 app.use(express.static(__dirname + "/public"));
 app.use('/public', express.static(__dirname + "/public"));
 
-
+app.get("/json", function(req, res) {
+    res.json(
+        {"message": "Hello json"}
+        );
+});
 
 
 

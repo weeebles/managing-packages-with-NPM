fc04222f02ed46d6277a415c57d04d66b9eea83e
bleesherman <becs.leesherman@gmail.com>
2022-07-02 23:38:11 -0500

    Add body parser plus function to run each route

--- myApp.js
+++ myApp.js
@@ -2,13 +2,17 @@ let express = require('express');
 let app = express();
 var bGround = require('fcc-express-bground');
 require('dotenv').config();
+var bodyParser = require('body-parser');
 
 app.use(function(req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
 
-
+app.use(function(req, res, next) {
+    bodyParser.urlencoded({extended: false});
+    next();
+});
 
 bGround.log("Hello World");
 console.log("Hello World");

67e139cbfe9bc38a70a59bef5d63a871a7a6879c
bleesherman <becs.leesherman@gmail.com>
2022-07-02 19:33:20 -0500

    Send index html file rather than string on root get request

--- server.js
+++ server.js
@@ -22,10 +22,40 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+// app.use('/public', express.static(process.cwd() + '/public'));
+
+// app.route('/_api/package.json')
+//   .get(function(req, res, next) {
+//     console.log('requested');
+//     fs.readFile(__dirname + '/package.json', function(err, data) {
+//       if(err) return next(err);
+//       res.type('txt').send(data.toString());
+//     });
+//   });
+  
+// app.route('/')
+//     .get(function(req, res) {
+//       res.sendFile(process.cwd() + '/views/index.html');
+//     })
+
+// // Respond not found to all the wrong routes
+// app.use(function(req, res, next){
+//   res.status(404);
+//   res.type('txt').send('Not found');
+// });
+
+// // Error Middleware
+// app.use(function(err, req, res, next) {
+//   if(err) {
+//     res.status(err.status || 500)
+//       .type('txt')
+//       .send(err.message || 'SERVER ERROR');
+//   }  
+// })
 
 
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 var port = process.env.PORT || 3000;

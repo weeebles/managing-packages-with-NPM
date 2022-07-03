1734fa70428ccdb0b1f852585c1ae1532844d8a1
bleesherman <becs.leesherman@gmail.com>
2022-07-02 20:07:47 -0500

    Changed function to arrows

--- myApp.js
+++ myApp.js
@@ -13,14 +13,14 @@ console.log("Hello World");
 
 
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use(express.static(__dirname + "/public"));
 app.use('/public', express.static(__dirname + "/public"));
 
-app.get("/json", function(req, res) {
+app.get("/json", (req, res) => {
     res.json(
         {"message": "Hello json"}
         );

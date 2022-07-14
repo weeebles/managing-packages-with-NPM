3663c669f9e8cdac47519fdff991e4e9dff39f91
bleesherman <becs.leesherman@gmail.com>
2022-07-14 13:02:01 -0500

    server.js file

--- server.js
+++ server.js
@@ -22,8 +22,6 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.use(express.static(__dirname + "/public"));
-app.use('/public', express.static(__dirname + "/public"));
 // app.use('/public', express.static(process.cwd() + '/public'));
 
 // app.route('/_api/package.json')
@@ -55,10 +53,6 @@ app.use('/public', express.static(__dirname + "/public"));
 //   }  
 // })
 
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
